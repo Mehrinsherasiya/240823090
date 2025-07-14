0b3742122baa639e901a54b51f2741203108bc7a
mehrinsherasiya <sherasiyamehrin@example.com>
2025-07-15 04:24:28 +0530

    node index

---
+++ index.js
@@ -0,0 +1,66 @@
+// var name='mehrin'
+// console.log("Hello "+name)
+
+// console.log("program.1 even odd number")
+// var n=4
+// if(n%2)
+// {
+//     console.log("odd number")
+// }
+// else
+// {
+//     console.log("even number")
+// }
+
+// console.log("program.2 find the maximum of two number")
+// var a=4
+// var b=10
+// if(a>b)
+// {
+//     console.log("maximum number of:",a)
+// }
+// else
+// {
+//     console.log("maximum number of:",b)
+// }
+
+// console.log("program.3 chek leap year")
+// var year=2004
+// if(year/4)
+// {
+//     console.log(year,"is leap year")
+// }
+// else
+// {
+//     console.log(year,"is not leap year")
+// }
+
+
+// console.log("program.4 sum of natrual number")
+// var j=5
+// sum=0
+// for(i=1;i<j;i++)
+// {
+//     sum=i
+// }
+// console.log(sum)
+
+
+// console.log("program.5 factorial of numner")
+// var a=5
+// var fact=1
+// for(i=1;i<=a;i++)
+// {
+//     fact*=i
+// }
+// console.log(fact)
+
+
+console.log("program.6 print multilication table")
+var a=3
+for(i=1;i<=10;i++)
+{
+    console.log(`${a} x ${i} = ${a*i}`)
+}
+
+console.log("program.7 revers a number")
